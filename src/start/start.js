9b412d333ac9ffc1bd9d55570e82558993f20a4a
thangdjw <thangphung.work@gmail.com>
2019-04-21 22:01:21 +0700

    fix bug

--- src/start/start.js
+++ src/start/start.js
@@ -1,11 +1,8 @@
-
-
 function runApp(path) {
     var extension = path.substr(path.lastIndexOf('.')+1);
     if (extension == "json") {
-        const hyron = require("hyron");
+        var hyron = require("hyron");
         hyron.build(path);
-        // eval(`const hyron = require("hyron");hyron.build(path);`)
 
     } else if (extension == "js") {
         require(path);
